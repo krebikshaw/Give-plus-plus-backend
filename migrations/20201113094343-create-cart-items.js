ca1c78698b6fb44c8bde99958f04b73b29e7b84c
Nicolas Cheng <60566603+Nicolakacha@users.noreply.github.com>
2020-11-26 21:18:20 +0800

    Update 20201113094343-create-cart-items.js

--- migrations/20201113094343-create-cart-items.js
+++ migrations/20201113094343-create-cart-items.js
@@ -12,7 +12,7 @@ module.exports = {
         allowNull: false,
         type: Sequelize.INTEGER,
       },
-      UerId: {
+      UserId: {
         allowNull: false,
         type: Sequelize.INTEGER,
       },
