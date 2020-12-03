2eccff0d9b3ff3870207322b1d391ab506e5176d
nicolakacha <nicolakacha@gmail.com>
2020-12-03 23:53:59 +0800

    add-seeder

---
+++ seeders/20201126094254-demo-cart-item.js
@@ -0,0 +1,37 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Cart_items', [
+      {
+        ProductId: 1,
+        UserId: 3,
+        CartId: 1,
+        product_quantity: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        ProductId: 5,
+        UserId: 4,
+        CartId: 1,
+        product_quantity: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+      {
+        ProductId: 8,
+        UserId: 5,
+        CartId: 1,
+        product_quantity: 1,
+        createdAt: new Date(),
+        updatedAt: new Date(),
+      },
+
+    ]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Cart_items', null, {});
+  },
+};
