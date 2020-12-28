f1a02ee858634deddeb8914efcaf543491be2104
nicolakacha <nicolakacha@gmail.com>
2020-12-28 13:54:16 +0800

    fix: add console.log

--- controllers/manageController.js
+++ controllers/manageController.js
@@ -17,6 +17,7 @@ const noDataMessage = {
 
 const manageController = {
   getAllRules: (_, res) => {
+    console.log('開始撈取所有條款');
     Rule.findAll()
       .then((rules) => {
         const rulesData = {
@@ -26,26 +27,29 @@ const manageController = {
             return { id, rule, content };
           }),
         };
+        console.log('結束撈取所有條款');
         return res.status(200).json(rulesData);
       })
       .catch((err) => {
-        console.log(err);
-        res.status(400).json(noDataMessage);
+        console.log('撈取所有條款錯誤：', err);
+        return res.status(400).json(noDataMessage);
       });
   },
 
   getRule: (req, res) => {
+    console.log('開始撈取條款');
     Rule.findByPk(req.params.id)
       .then((ruleData) => {
         const { id, rule, content } = ruleData;
+        console.log('結束撈取條款：', ruleData);
         return res.status(200).json({
           ok: 1,
           data: { id, rule, content },
         });
       })
       .catch((err) => {
-        console.log(err);
-        res.status(400).json(noDataMessage);
+        console.log('撈取單一條款錯誤：', err);
+        return res.status(400).json(noDataMessage);
       });
   },
 
@@ -53,12 +57,11 @@ const manageController = {
     const { rule, content } = req.body;
     if (!rule || !content || rule.trim() === '' || content.trim() === '')
       return res.status(400).json(emptyErrorMessage);
-
     Rule.create({ rule, content })
       .then(() => res.status(200).json(successMessage))
       .catch((err) => {
         console.log(err);
-        res.status(400).json({ ok: 0, message: err });
+        return res.status(400).json({ ok: 0, message: err });
       });
   },
 
@@ -75,7 +78,7 @@ const manageController = {
       .then(() => res.status(200).json(successMessage))
       .catch((err) => {
         console.log(err);
-        res.status(400).json({ ok: 0, message: err });
+        return res.status(400).json({ ok: 0, message: err });
       });
   },
 
@@ -88,11 +91,12 @@ const manageController = {
       .then(() => res.status(200).json(successMessage))
       .catch((err) => {
         console.log(err);
-        res.status(400).json({ ok: 0, message: err });
+        return res.status(400).json({ ok: 0, message: err });
       });
   },
 
   getAllFaqs: (_, res) => {
+    console.log('開始撈取所有常見問題');
     Faq_categories.findAll({ include: Faq })
       .then((faqCategories) => {
         const faqList = [];
@@ -114,15 +118,17 @@ const manageController = {
           ok: 1,
           data: faqList,
         };
+        console.log('所有常見問題：', faqsData);
         return res.status(200).json(faqsData);
       })
       .catch((err) => {
-        console.log(err);
-        res.status(400).json({ ok: 0, message: err });
+        console.log('撈取所有常見問題錯誤：', faqsData);
+        return res.status(400).json({ ok: 0, message: err });
       });
   },
 
   getFaq: (req, res) => {
+    console.log('開始撈取單一常見問題');
     Faq.findOne({
       where: {
         id: req.params.id,
@@ -132,13 +138,14 @@ const manageController = {
       .then((faq) => {
         const { id, question, answer } = faq;
         const category = faq.Faq_category.name;
+        console.log('單一常見問題：', category);
         return res.status(200).json({
           ok: 1,
           data: { id, category, question, answer },
         });
       })
       .catch((err) => {
-        console.log(err);
+        console.log('撈取單一常見問題失敗：', category);
         res.status(400).json(noDataMessage);
       });
   },
@@ -163,9 +170,7 @@ const manageController = {
 
     Faq.create({ question, answer, faqCategoryId })
       .then(() => res.status(200).json(successMessage))
-      .catch((err) => {
-        res.status(400).json({ ok: 0, message: err });
-      });
+      .catch((err) => res.status(400).json({ ok: 0, message: err }));
   },
 
   editFaq: async (req, res) => {
@@ -186,9 +191,7 @@ const manageController = {
     faqData
       .update({ question, answer, faqCategoryId })
       .then(() => res.status(200).json(successMessage))
-      .catch((err) => {
-        res.status(400).json({ ok: 0, message: err });
-      });
+      .catch((err) => res.status(400).json({ ok: 0, message: err }));
   },
 
   deleteFaq: async (req, res) => {
@@ -197,12 +200,11 @@ const manageController = {
     faqData
       .destroy()
       .then(() => res.status(200).json(successMessage))
-      .catch((err) => {
-        res.status(400).json({ ok: 0, message: err });
-      });
+      .catch((err) => res.status(400).json({ ok: 0, message: err }));
   },
 
   getAllFaqCategories: (_, res) => {
+    console.log('開始撈取所有常見問題分類');
     Faq_categories.findAll()
       .then((faqCategories) => {
         const faqCategoriesData = {
@@ -212,11 +214,12 @@ const manageController = {
             return { id, name };
           }),
         };
+        console.log('所有常見問題分類：', faqCategoriesData);
         return res.status(200).json(faqCategoriesData);
       })
       .catch((err) => {
-        console.log(err);
-        res.status(400).json(noDataMessage);
+        console.log('撈取所有常見問題分類錯誤：', err);
+        return res.status(400).json(noDataMessage);
       });
   },
 
@@ -227,9 +230,7 @@ const manageController = {
 
     Faq_categories.create({ name })
       .then(() => res.status(200).json(successMessage))
-      .catch((err) => {
-        res.status(400).json({ ok: 0, message: err });
-      });
+      .catch((err) => res.status(400).json({ ok: 0, message: err }));
   },
 
   editFaqCategory: async (req, res) => {
@@ -243,9 +244,7 @@ const manageController = {
     faqCategoryData
       .update({ name })
       .then(() => res.status(200).json(successMessage))
-      .catch((err) => {
-        res.status(400).json({ ok: 0, message: err });
-      });
+      .catch((err) => res.status(400).json({ ok: 0, message: err }));
   },
 
   deleteFaqCategory: async (req, res) => {
@@ -261,12 +260,11 @@ const manageController = {
     faqCategoryData
       .destroy()
       .then(() => res.status(200).json(successMessage))
-      .catch((err) => {
-        res.status(400).json({ ok: 0, message: err });
-      });
+      .catch((err) => res.status(400).json({ ok: 0, message: err }));
   },
 
   getAllMails: (_, res) => {
+    console.log('開始撈取所有聯絡信件');
     Mail.findAll()
       .then((mails) => {
         const mailsData = {
@@ -276,26 +274,29 @@ const manageController = {
             return { id, name, email, content };
           }),
         };
+        console.log('所有聯絡信件：', mailsData);
         return res.status(200).json(mailsData);
       })
       .catch((err) => {
-        console.log(err);
-        res.status(400).json({ ok: 0, message: err });
+        console.log('撈取所有聯絡信件錯誤：', err);
+        return res.status(400).json({ ok: 0, message: err });
       });
   },
 
   getMail: (req, res) => {
+    console.log('開始撈取單一聯絡信件');
     Mail.findByPk(req.params.id)
       .then((mail) => {
         const { id, name, email, content } = mail;
+        console.log('單一聯絡信件：', mail);
         return res.status(200).json({
           ok: 1,
           data: { id, name, email, content },
         });
       })
       .catch((err) => {
-        console.log(err);
-        res.status(400).json(noDataMessage);
+        console.log('撈取單一聯絡信件錯誤：', err);
+        return res.status(400).json(noDataMessage);
       });
   },
 
@@ -310,7 +311,8 @@ const manageController = {
       .update({ status })
       .then(() => res.status(200).json(successMessage))
       .catch((err) => {
-        res.status(400).json({ ok: 0, message: err });
+        console.log('編輯商品狀態錯誤：', err);
+        return res.status(400).json({ ok: 0, message: err });
       });
   },
 };
